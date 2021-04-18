5b59ddea8e1977e7a2dfa00605207a4d8e543de9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-04-19 03:52:57 +0700

    Add new route for validasi pendaftar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,7 @@ $routes->get('/', 'Auth::login');
 $routes->get('/admin', 'Admin::dashboard');
 $routes->get('/pendaftar/tambah-pendaftar', 'Pendaftar::tambahPendaftar');
 $routes->get('/pendaftar/data-pendaftar', 'Pendaftar::dataPendaftar');
-$routes->get('/pendaftar/validasi-pendaftar', 'Pendaftar::tambahPendaftar');
+$routes->get('/pendaftar/validasi-pendaftar', 'Pendaftar::validasiPendaftar');
 
 /*
  * --------------------------------------------------------------------
