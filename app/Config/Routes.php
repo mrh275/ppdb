445ce38767349014028a8840d2dcbff1d0b16ebf
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-04-19 03:52:08 +0700

    Add new route for tambah pendaftar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Auth::login');
 $routes->get('/admin', 'Admin::dashboard');
+$routes->get('/pendaftar/tambah-pendaftar', 'Pendaftar::tambahPendaftar');
 
 /*
  * --------------------------------------------------------------------
