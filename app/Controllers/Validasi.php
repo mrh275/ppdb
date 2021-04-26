de0bff744ba1dae0c5798eeff48f5a130a388562
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-04-26 16:20:25 +0700

    Make Validasi Pendaftar Menu to be works

--- app/Controllers/Validasi.php
+++ app/Controllers/Validasi.php
@@ -8,8 +8,13 @@ class Validasi extends BaseController
 {
 	public function pendaftar()
 	{
+		$submenu = 'validasi-pendaftar';
 		$data = [
 			'title' => 'Validasi Pendaftar | PPDB SMA Negeri 1 Rawamerta',
+			'slug'			=> 'validasi-pendaftar',
+			'listMenu'      => $this->menuModel->getMenu(),
+			'submenuSlug'   => $submenu,
+			'submenuName'	=> $this->menuModel->getSubmenuBy($submenu),
 		];
 
 		return view('front/validasi-pendaftar', $data);
