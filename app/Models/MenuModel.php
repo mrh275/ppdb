fb3d3bfacbee830e45b315cc3aff7c02de0f4b1b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-04-26 16:21:01 +0700

    Create getSubmenu method to fetch submenu from database

--- app/Models/MenuModel.php
+++ app/Models/MenuModel.php
@@ -10,4 +10,20 @@ class MenuModel extends Model
 	{
 		return $this->db->table('menu')->get()->getResultArray();
 	}
+
+	public function getSubmenuBy($submenu)
+	{
+		return $this->db->table('submenu')->where(['slug_submenu' => $submenu])->get()->getRowArray();
+	}
+
+	public function getSubMenu($menuId)
+	{
+		$querySubMenu = "SELECT *
+                FROM `submenu` JOIN `menu`
+                ON `submenu`.`menu_id` = `menu`.`id`
+                WHERE `submenu`.`menu_id` = $menuId
+                AND `submenu`.`active` = 1
+            ";
+		return $this->db->query($querySubMenu)->getResultArray();
+	}
 }
