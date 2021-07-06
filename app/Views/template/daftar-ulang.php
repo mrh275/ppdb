ba3260d8a4ca69176953a0e8a7a5a07925997795
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-06 21:44:53 +0700

    Add new template layout for daftar ulang page

---
+++ app/Views/template/daftar-ulang.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+
+    <!-- Style Assets -->
+    <link rel="stylesheet" href="<?= base_url('asset/bootstrap/css') ?>/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/fontawesome/css') ?>/all.css">
+    <link rel="stylesheet" href="<?= base_url('asset/node_modules/sweetalert2/dist') ?>/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/css') ?>/style.css">
+
+    <!-- JS Assets -->
+    <script src="<?= base_url('asset/js') ?>/jquery.min.js"></script>
+    <script src="<?= base_url('asset/node_modules/@popperjs/core/dist/umd') ?>/popper.min.js"></script>
+    <script src="<?= base_url('asset/bootstrap/js') ?>/bootstrap.min.js"></script>
+    <script src="<?= base_url('asset/fontawesome/js') ?>/all.js"></script>
+    <script src="<?= base_url('asset/node_modules/sweetalert2/dist') ?>/sweetalert2.all.min.js"></script>
+
+</head>
+
+<body>
+
+    <?= $this->renderSection('content'); ?>
+
+</body>
+
+</html>
