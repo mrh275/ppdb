e2f800b80281c43f492e3a26b6f64af6d5822d4a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-08 21:14:26 +0700

    Create template for pdf export

---
+++ app/Views/template/bukti-pdf.php
@@ -0,0 +1,140 @@
+<div class="row mt-3">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-10">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-borderless">
+                    <tbody>
+                        <tr class="head-bukti">
+                            <td class="d-flex head-bukti justify-content-center align-content-center"><img src="<?= base_url('asset/img') ?>/logo-big.png" alt="Logo SMA Negeri 1 Rawamerta" class="logo"></td>
+                            <td class="text-center head-bukti" colspan="2">
+                                <p>PEMERINTAH DAERAH PROVINSI JAWA BARAT</p>
+                                <p>DINAS PENDIDIKAN</p>
+                                <p>CABANG DINAS PENDIDIKAN WILAYAH IV</p>
+                                <p>SMA NEGERI 1 RAWAMERTA</p>
+                                <p>Jl. Garunggung - Panyingkiran Kec. Rawamerta Kab. Karawang, 41382</p>
+                                <p>Email : sman1rwt@gmail.com. Website: https://sman1rawamerta.sch.id</p>
+                            </td>
+                        </tr>
+                        <tr class="mt-2">
+                            <td colspan="3">
+                                <img src="<?= base_url('asset/img') ?>/logo-super.png" alt="Watermark" class="watermark">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" class="text-center align-middle">
+                                <h6 class="font-weight-bold mt-4">
+                                    PENERIMAAN PESERTA DIDIK BARU <br>
+                                    TAHUN PELAJARAN 2021/2022
+                                </h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" class="text-center align-middle">
+                                <h6 class="font-weight-bold mt-3 mb-4">
+                                    TANDA BUKTI PENDAFTARAN
+                                </h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Nomor Pendaftaran
+                            </td>
+                            <td>:</td>
+                            <td>
+                                PPDB-1031
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                NISN
+                            </td>
+                            <td>:</td>
+                            <td>
+                                9937836287
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Nama Lengkap
+                            </td>
+                            <td>:</td>
+                            <td>
+                                Muhamad Ramdani Hidayatullah
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Tempat, Tanggal Lahir
+                            </td>
+                            <td>:</td>
+                            <td>
+                                Karawang, 30 Januari 2021
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Jenis Kelamin
+                            </td>
+                            <td>:</td>
+                            <td>
+                                Laki - laki
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Asal Sekolah
+                            </td>
+                            <td>:</td>
+                            <td>
+                                MTs Negeri Jatisari
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Nama Ibu Kandung
+                            </td>
+                            <td>:</td>
+                            <td>
+                                Siti Nursoleha
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Nilai Akhir
+                            </td>
+                            <td>:</td>
+                            <td>
+                                83.2
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="font-weight-bold">
+                                Alamat Lengkap
+                            </td>
+                            <td>:</td>
+                            <td>
+                                <p style="max-width: 500px; width: 100%;">
+                                    Perum Bumi Cikampek Baru Blok 8.8 No. 08 RT. 10 RW. 08 Ds. Balonggandu Kec. Jatisari Kab. Karawang
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"></td>
+                            <td>
+                                <p class="mt-3">
+                                    Karawang, 11 Juni 2021 <br>
+                                    Kepala SMA Negeri 1 Rawamerta <br>
+                                    <img src="<?= base_url('asset/img') ?>/ppdb.svg"> <br>
+                                    <strong>Drs. Asep Mamun, M.Pd.</strong> <br>
+                                    NIP. 19660629 199306 1 001
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-1"></div>
+</div>
