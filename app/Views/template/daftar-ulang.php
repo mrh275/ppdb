6c67fa178a77cd0b1ff1a8f9f1237ed837edb73f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-08 21:14:53 +0700

    Add footer and some js dependecies

--- app/Views/template/daftar-ulang.php
+++ app/Views/template/daftar-ulang.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="<?= base_url('asset/fontawesome/css') ?>/all.css">
     <link rel="stylesheet" href="<?= base_url('asset/node_modules/sweetalert2/dist') ?>/sweetalert2.min.css">
     <link rel="stylesheet" href="<?= base_url('asset/css') ?>/style.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
 
     <!-- JS Assets -->
     <script src="<?= base_url('asset/js') ?>/jquery.min.js"></script>
@@ -19,12 +20,25 @@
     <script src="<?= base_url('asset/bootstrap/js') ?>/bootstrap.min.js"></script>
     <script src="<?= base_url('asset/fontawesome/js') ?>/all.js"></script>
     <script src="<?= base_url('asset/node_modules/sweetalert2/dist') ?>/sweetalert2.all.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js"></script>
 
 </head>
 
 <body>
 
     <?= $this->renderSection('content'); ?>
+
+    <footer class="main-footer">
+        <div class="container-fluid bg-dark text-white mt-4" style="padding:10px 15px 10px 15px">
+            <strong>Copyright &copy; 2021 - <?= date('Y') ?> <a href="https://sman1rawamerta.sch.id" target="_blank">SMA Negeri 1 Rawamerta</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+                Made with <i class="fas fa-heart" style="color: red;"></i> by Muhamad Ramdani Hidayatullah
+            </div>
+        </div>
+    </footer>
 
 </body>
 
