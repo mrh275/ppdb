28ba0cbcfecfd6bb06c3a117a4fe872b48932b4d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-08 21:12:51 +0700

    Create new page for result daftar ulang

---
+++ app/Views/front/daftar-ulang-done.php
@@ -0,0 +1,42 @@
+<?= $this->extend('template/daftar-ulang') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+    <div class="row header-hasil-seleksi mt-3">
+        <div class="col-md-12 d-flex text-center justify-content-center">
+            <img src="<?= base_url('asset/img') ?>/logo-big.png" alt="Logo SMAN 1 Rawamerta" class="brand-daftar-ulang" style="width: 70px;">
+        </div>
+    </div>
+    <div class="row header-hasil-seleksi mt-3">
+        <div class="col-md-12 text-center">
+            <h4 class="daftar-ulang-ppdb">
+                PENERIMAAN PESERTA DIDIK BARU<br>
+                SMA NEGERI 1 RAWAMERTA<br>
+                TAHUN PELAJARAN 2021/2022
+            </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h4 class="hasil-seleksi">
+                DAFTAR ULANG<br>
+                TAHAP 2<br>
+            </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h2 class="daftar-ulang-done">
+                PROSES DAFTAR ULANG TAHAP 2<br>
+                SELESAI
+            </h2>
+            <h2 class="daftar-ulang-done">
+                SELAMAT DATANG SISWA BARU<br>
+                SMA NEGERI 1 RAWAMERTA
+            </h2>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
