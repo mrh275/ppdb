994c82dd78a22ab8ceb7eaf1994418f2214f3591
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-10 13:28:38 +0700

    Create view for waiting list

---
+++ app/Views/front/belum-dibuka.php
@@ -0,0 +1,39 @@
+<?= $this->extend('template/daftar-ulang') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+    <div class="row header-hasil-seleksi mt-3">
+        <div class="col-md-12 d-flex text-center justify-content-center">
+            <img src="<?= base_url('asset/img') ?>/logo-big.png" alt="Logo SMAN 1 Rawamerta" class="brand-daftar-ulang" style="width: 70px;">
+        </div>
+    </div>
+    <div class="row header-hasil-seleksi mt-3">
+        <div class="col-md-12 text-center">
+            <h4 class="daftar-ulang-ppdb">
+                PENERIMAAN PESERTA DIDIK BARU<br>
+                SMA NEGERI 1 RAWAMERTA<br>
+                TAHUN PELAJARAN 2021/2022
+            </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h4 class="hasil-seleksi">
+                HASIL SELEKSI<br>
+                TAHAP 2<br>
+            </h4>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-12 text-center">
+            <h2 class="sabar">
+                SABAR YA!<br>
+                HALAMAN HASIL SELEKSI BELUM DIBUKA<br>
+                NANTI AKAN DIBUKA PUKUL 10.00 WIB
+            </h2>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
