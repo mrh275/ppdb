ef18a888afbb531fc8155c22b1d71dcfd2fb3250
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-07-11 16:37:15 +0700

    Added new method to fetch data from database if exist

--- app/Models/DaftarUlangModel.php
+++ app/Models/DaftarUlangModel.php
@@ -15,4 +15,34 @@ class DaftarUlangModel extends Model
     {
         return $this->db->table('hasil_seleksi')->get()->getResultArray();
     }
+
+    public function getBiodata($noRegis)
+    {
+        return $this->db->table('biodata_pendaftar')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
+
+    public function getDataAyah($noRegis)
+    {
+        return $this->db->table('data_ayah')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
+
+    public function getDataIbu($noRegis)
+    {
+        return $this->db->table('data_ibu')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
+
+    public function getDataWali($noRegis)
+    {
+        return $this->db->table('data_wali')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
+
+    public function getDataPeriodik($noRegis)
+    {
+        return $this->db->table('data_periodik')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
+
+    public function getDataUpload($noRegis)
+    {
+        return $this->db->table('data_upload')->where(['no_regis' => $noRegis])->get()->getRowArray();
+    }
 }
